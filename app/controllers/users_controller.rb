ae1f8e136e333c00a95e928df40a4314c515c306
sidazhang <sidazhang89@gmail.com>
2013-05-22 17:17:39 -0700

    add static pages, user controller and matching views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(params[:user])
+    redirect_to user_path(@user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Surveyor::Application.routes.draw do
+  root :to => 'static_pages#home'
+  resources :users
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,6 @@ Surveyor::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
